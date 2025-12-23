a6fc930b775ff645cffcc25c5b55ce62a3929b64
Patricia Rocha Rufino <patricia.rocha@estudante.ufjf.br>
2025-12-23 10:36:33 -0300

    Adiciona documentação inicial do módulo de monitoramento

--- src/monitoramento.py
+++ src/monitoramento.py
@@ -2,3 +2,15 @@
 Módulo de monitoramento em tempo real do sistema pneumático.
 Responsável pela leitura das variáveis do processo.
 """
+class monitoramento(object):
+    "classe responsável pelo monitoramento das variáveis do sistema pneumático"
+    
+    def __init__(self):
+        "Incializa as variáveis"
+        
+        self.variaveis = {}
+
+    def atualizar_variavel(self, nome, valor):
+        "Atualiza o valor de uma variável monitorada"
+
+        self.variaveis[nome] = valor
