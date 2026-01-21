195da45e242843fdfbb7f76a42a3864a65b09dec
PatriciaRoR <patricia.rocha@estudante.ufjf.br>
2026-01-21 16:10:44 -0300

    Refactor comments for consistency and clarity

--- src/Core/RealTimeGraph.py
+++ src/Core/RealTimeGraph.py
@@ -21,23 +21,23 @@ class RealTimeGraphPopup(ModalView):
             **kwargs   #Evita fechamento acidental
         )
 
-        # 🔹 parâmetros
+        # parâmetros
         self.main_widget = main_widget
         self.var_attr = var
         self.unit = unit
         self.name = name
         self.max_value = max_value
 
-        # 🔹 tempo e dados
+        #  tempo e dados
         self.start_time = datetime.now()
         self.max_points = 20
         self.plot_points = deque(maxlen=self.max_points)
 
-        # 🔹 conteúdo visual (KV)
+        # conteúdo visual (KV)
         self.content = RealTimeGraphContent()
         self.add_widget(self.content)
 
-        # 🔹 inicialização tardia (IMPORTANTE)
+        # inicialização tardia (IMPORTANTE)
         self.graph = None
         self.plot = None
 
@@ -80,12 +80,12 @@ class RealTimeGraphPopup(ModalView):
         self.plot_points.append((elapsed, value))
         self.plot.points = list(self.plot_points)
 
-        # 🔥 nunca corta valor real
+        # nunca corta valor real
         if value >= self.graph.ymax:
             self.graph.ymax = value * 1.2
             self.graph.y_ticks_major = max(self.graph.ymax / 5, 1)
 
-        # 🔥 eixo X acompanha o tempo
+        # eixo X acompanha o tempo
         if elapsed >= self.graph.xmax:
             self.graph.xmax = elapsed + self.main_widget.scan_time * 5
             self.graph.x_ticks_major = self.graph.xmax / 5
